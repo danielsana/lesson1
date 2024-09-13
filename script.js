1d96c803ca8a9ab94a01fdd8dcf61ab0f9aa57e9
dansana <danielsana60@gmail.com>
2024-09-13 21:40:09 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+// varibleas
+let myname = "John Doe";
+console.log(myname);
+
+let age = 30;
+age = 35;
+const gender = "male";
+
+console.log(myname);
+console.log(age);
+console.log(gender);
+
+// data types
+// numbers
+let temp = 36.5;
+// console.log(temp)
+// console.log(typeof(temp))
+
+// strings (series of characters)
+// let message = "Hello, World!"
+// let student = 'john'
+// console.log(message)
+// console.log(typeof(message))
+// console.log(student)
+// console.log(typeof(student))
+
+// BOOLEAN
+let num1 = 20;
+let num2 = 35;
+let isAdmin = true;
+let isGreater = num1 > num2;
+// console.log(isAdmin)
+// console.log(typeof(isAdmin))
+// console.log(isGreater)
+// console.log(typeof(isGreater))
+
+// array
